d2c46d58f6a40d345a162fc07a08239bc9276c9a
Davide Zago <dawoz.98@gmail.com>
2022-06-23 19:14:24 +0200

    Updates

--- src/app/query1/query1.component.ts
+++ src/app/query1/query1.component.ts
@@ -1,24 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {SparqlService} from "../sparql/sparql.service";
+import {RDFData, SparqlService} from "../sparql/sparql.service";
 
 @Component({
   selector: 'app-query1',
   templateUrl: './query1.component.html',
   styleUrls: ['./query1.component.css']
 })
+
+
+
 export class Query1Component implements OnInit {
-  bands: string[] | undefined
-  bandsUri: string[] | undefined
-  selectedBandUri: string | undefined
+  bands: Array<string> | undefined
+  bandsUri: Array<string> | undefined
+  selectedIndex: number | undefined
+  albums: Array<RDFData> | undefined
+  tracks: Record<string,RDFData[]> = {}
 
   constructor(private sparql: SparqlService) { }
 
   ngOnInit(): void {
     this.sparql.getBands().subscribe(d => {
-        // @ts-ignore
       this.bandsUri = d.map<string>(e => e['band']);
-        // @ts-ignore
-      this.bands = d.map<string>(e => e['band'].split('#').at(-1).replaceAll('_', ' '));
+      this.bands = d.map<string>(e => e['bandLabel']);
+    })
+  }
+
+  queryAlbums() {
+    this.sparql.query1(this.bandsUri![this.selectedIndex!]).subscribe(albums => {
+      this.albums = albums
+      for (let a of albums) {
+        this.sparql.getTracksOf(a['album']).subscribe(tracks => {
+            this.tracks[a['album']] = tracks
+          }
+        )
+      }
     })
   }
 }
--- src/app/sparql/sparql.service.ts
+++ src/app/sparql/sparql.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {map, Observable} from "rxjs";
 
+export type RDFData = Record<string, string>
+
 @Injectable({
   providedIn: 'root'
 })
 export class SparqlService {
   private endpointUrl = 'http://localhost:7200/repositories/ipro'
   private prefix =
-    `prefix : <http://ipro-semantic.com#>
+    `prefix : <http://www.semanticweb.org/ipro#>
     prefix dc: <http://purl.org/dc/elements/1.1/>
     prefix owl: <http://www.w3.org/2002/07/owl#>
     prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -23,7 +25,7 @@ export class SparqlService {
   constructor(private http: HttpClient) {
   }
 
-  query(q: string): Observable<any[]> {
+  query(q: string): Observable<Array<RDFData>> {
     const params = new HttpParams()
       .set('query', this.prefix + '\n' + q)
       .set('format', 'json');
@@ -39,20 +41,58 @@ export class SparqlService {
         .map((elem: any) => JSON.parse('{' + Object.keys(elem).map(k => ` "${k}" : "${elem[k].value}" `) + '}'))))
   }
 
-  public getBands(): Observable<Array<object>> {
+  public getBands(): Observable<Array<RDFData>> {
     const q = `
-      select ?band  where {
-        ?band a :Band .
+      select ?band ?bandLabel where {
+        ?band a :Band ;
+              rdfs:label ?bandLabel .
       }`
     return this.query(q)
   }
 
-  // get members of bands
-  public query1(bandUri: string): Observable<Array<object>> {
+  // get albums of bands
+  public query1(bandUri: string): Observable<Array<RDFData>> {
     const q = `
-      select ?band ?member  where {
-        ${bandUri} :hasMember ?member .
+      select ?album ?albumLabel ?year ?albumComment ?imageUrl {
+        <${bandUri}> :discography [
+          :containsRelease ?release
+        ] .
+        ?release :ofAlbum ?album ;
+                 :releaseYear ?year .
+        ?album rdfs:label ?albumLabel ;
+               rdfs:comment ?albumComment ;
+               :imageUrl ?imageUrl .
       }`
+    return this.query(q)
+  }
+
+  // get tracks of an album
+  public getTracksOf(albumUri: string): Observable<Array<RDFData>> {
+    const q = `
+      select ?n ?trackLabel ?minutes ?seconds {
+        ?track :trackOf <${albumUri}> ;
+               :trackNumber ?n ;
+               rdfs:label ?trackLabel ;
+               time:hasDuration [
+                    time:minutes ?minutes ;
+                    time:seconds ?seconds
+                    ] .
+      }
+    `
+    return this.query(q)
+  }
+
+  // get tracks of an album
+  public getGenresOf(albumUri: string): Observable<Array<RDFData>> {
+    const q = `
+      select ?genreLabel ?genreComment {
+        <${albumUri}> :hasGenre [
+            rdfs:comment ?genreComment ;
+           rdfs:label ?genreLabel .
+        ]
+      }
+      order by ?trackNumber
+    `
     return this.query(q)
   }
 }
